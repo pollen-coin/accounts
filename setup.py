c23e2c9d6164a94b589c206bc1ae89d7f29bc3ba
Sean Geiger <sean@geigers.org>
2017-12-21 21:29:29 -0800

    v1.0.1

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='pollen_access',
-      version='1.0',
+      version='1.0.1',
       description='Python interface for Pollen coin wallet RPC',
       url='https://github.com/pollen-coin/pollen_access',
       author='seangeiger',
